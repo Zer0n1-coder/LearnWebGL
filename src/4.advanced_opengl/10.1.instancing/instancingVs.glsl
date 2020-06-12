f6c06ea51ac24281efce3867e729457eabec30e1
CheRui <850989926@qq.com>
2020-06-12 12:02:13 +0800

    update 10.1.instancing

---
+++ src/4.advanced_opengl/10.1.instancing/instancingVs.glsl
@@ -0,0 +1,13 @@
+#version 300 es
+
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aOffset;
+
+out vec3 fColor;
+
+void main(){
+    vec2 pos = aPos * (float(gl_InstanceID) / 100.0);
+    fColor = aColor;
+    gl_Position = vec4(pos + aOffset, 0.0,1.0);
+}
---
+++ src/4.advanced_opengl/10.1.instancing/main.js
@@ -0,0 +1,59 @@
+import { gl } from "./Global";
+import { Shader } from "./Shader";
+export function main() {
+    gl.enable(gl.DEPTH_TEST);
+    let shader = new Shader("./instancingVs.glsl", "./instancingFs.glsl");
+    let offset = new Float32Array(200);
+    let index = 0;
+    let bOffset = 0.1;
+    for (let y = -10; y < 10; y += 2) {
+        for (let x = -10; x < 10; x += 2) {
+            offset[index++] = x / 10 + bOffset;
+            offset[index++] = y / 10 + bOffset;
+        }
+    }
+    let instanceVBO = gl.createBuffer();
+    if (!instanceVBO) {
+        alert("creating instanceVBO is failed!");
+        return;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, instanceVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, offset, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    let quadVertices = new Float32Array([
+        -0.05, 0.05, 1.0, 0.0, 0.0,
+        0.05, -0.05, 0.0, 1.0, 0.0,
+        -0.05, -0.05, 0.0, 0.0, 1.0,
+        -0.05, 0.05, 1.0, 0.0, 0.0,
+        0.05, -0.05, 0.0, 1.0, 0.0,
+        0.05, 0.05, 0.0, 1.0, 1.0
+    ]);
+    let quadVAO = gl.createVertexArray();
+    if (!quadVAO) {
+        alert("creating quadVAO is failed!");
+        return;
+    }
+    let quadVBO = gl.createBuffer();
+    if (!quadVBO) {
+        alert("creating quadVBO is failed!");
+        return;
+    }
+    gl.bindVertexArray(quadVAO);
+    gl.bindBuffer(gl.ARRAY_BUFFER, quadVBO);
+    gl.bufferData(gl.ARRAY_BUFFER, quadVertices, gl.STATIC_DRAW);
+    gl.enableVertexAttribArray(0);
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.enableVertexAttribArray(1);
+    gl.vertexAttribPointer(1, 3, gl.FLOAT, false, 5 * Float32Array.BYTES_PER_ELEMENT, 2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(2);
+    gl.bindBuffer(gl.ARRAY_BUFFER, instanceVBO);
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    gl.vertexAttribDivisor(2, 1);
+    gl.clearColor(0.1, 0.1, 0.1, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    shader.use();
+    gl.bindVertexArray(quadVAO);
+    gl.drawArraysInstanced(gl.TRIANGLES, 0, 6, 100);
+    gl.bindVertexArray(null);
+}
