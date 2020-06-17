7823f3156944a9f132985c9c0db3c70d601af4e6
CheRui <850989926@qq.com>
2020-06-17 11:28:14 +0800

    update in_practice

--- src/7.in_practice/Global.ts
+++ src/7.in_practice/Global.ts
@@ -1,36 +1,7 @@
-﻿
-function getWebGL() {
-    let canvas = <HTMLCanvasElement>document.getElementById('webgl');
-
-    let tmpContext = canvas.getContext('webgl2', { depth:true,stencil: true,antialias:true });      //开启模板缓存
-    if (!tmpContext) {
-        window.alert('Failed to get the rendering context for WebGL');
-    }
-    return tmpContext!;
-}
-
-export let gl = getWebGL();
-
-export function loadTexture(id: string, type: number) {
-    let texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
-    //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);  //https://www.khronos.org/webgl/wiki/WebGL_and_OpenGL_Differences#Non-Power_of_Two_Texture_Support
-    //gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE); //采用和教程不同得形式原因，详见上面得网址
-    //gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-    let img = <HTMLImageElement>document.getElementById(id);
-    gl.texImage2D(gl.TEXTURE_2D, 0, type, type, gl.UNSIGNED_BYTE, img);
-    gl.generateMipmap(gl.TEXTURE_2D);
-
-    return texture;
-}
+export let canvas = <HTMLCanvasElement>document.getElementById("webgl");
+export let gl = <WebGL2RenderingContext>canvas.getContext("webgl2");
+export const SCR_WIDTH = canvas.width;
+export const SCR_HEIGHT = canvas.height;
 
 export function getTextFromLocation(path: string) {
     let request = new XMLHttpRequest;
@@ -39,4 +10,25 @@ export function getTextFromLocation(path: string) {
     request.send();
 
     return request.responseText;
+}
+
+export function loadTexture(id: string, type: number) {
+    let texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);  //https://www.khronos.org/webgl/wiki/WebGL_and_OpenGL_Differences#Non-Power_of_Two_Texture_Support
+    // gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE); //采用和教程不同得形式原因，详见上面得网址
+    // gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    let img = <HTMLImageElement>document.getElementById(id);
+    gl.texImage2D(gl.TEXTURE_2D, 0, type, type, gl.UNSIGNED_BYTE, img);
+    gl.generateMipmap(gl.TEXTURE_2D);
+
+    return texture;
 }
